9546a76f184e4d38d51c92dbf62aad88c8d58cd0
Yuri Oliveira <yurileader@gmail.com>
2021-12-14 11:49:29 -0300

    Criando criando todo-list e estilizando

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="logo">
+      <img
+        id="logoTodo"
+        class="img-fluid"
+        src="../src/assets/logo.png"
+        alt="Logo marca Todo"
+      />
+    </div>
+    <form @submit.prevent="SalvarTodo(todo)">
+      <div class="input-group mt-5">
+        <input
+          type="text"
+          v-model="todo.descricao"
+          class="form-control text-center"
+          placeholder="Adicione atividades na lista "
+        />
+        <button class="btn btn-primary" type="submit" id="adicionar">
+          Adicionar
+        </button>
+      </div>
+    </form>
+    <div class="a grid mt-5">
+      <div class="d-flex justify-content-center" v-for="t in todos" :key="t.id">
+        <div class="">
+          <i class="bi bi-clock "></i>
+        </div>
+        <div class="ps-3 col-8 d-flex">
+          <p>{{ t.id}}</p>
+          <p>{{ t.descricao }}</p>
+        </div>
+        <div class="col-2">
+          <button type="button" class="btn  link-danger" id="concluir">
+          Concluir
+        </button>
+        <button type="button" class="btn link-primary ms-1" id="excrluir">
+          Excluir
+        </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return { todos: [], todo: { checked: false, descricao: "" } };
+  },
+  methods: {
+    SalvarTodo(todo) {
+      todo.id = Date.now();
+      this.todos.push(todo);
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+@import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+.logo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+#logoTodo {
+  width: 90px;
+  height: 90px;
+  align-content: center;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+   <ul class="list-group">
+  <li v-for="todos in listaTodos" :key="todos.id" :itemprop="todo" class="list-group-item list-group-item-dark">{{todos.descricao}} {{todos.descricao}}</li>
+</ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  props: {
+    listaTodos: [],
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
